079ff3104f4192c6c7e2890824b7b2c58e0dc2b1
Guido Lippi <guidolippi94@gmail.com>
2022-06-09 17:03:09 +0200

    added test route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  res.send("QR-code generator API up and running");
+});
+
+app.get("/generate", (req, res) => {
   //   res.send("Hello World!");
   const inputUrl = req.query.url;
 
